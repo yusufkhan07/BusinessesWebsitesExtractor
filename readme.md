90f2d252da513964f467122d188d458bab3f451f
Yousuf Khan <yusufkhanjee@gmail.com>
2020-03-25 23:06:56 +0500

    Code refactor & readme updated

--- readme.md
+++ readme.md
@@ -1,5 +1,35 @@
-Config:
+# Database
 
-- Set Google API key on line 10
-- Sequelize object between line 12-18 can be used to configure mysql connection
-- lat,long & radius can be set in the main() function.
+We are using sequelize ORM for connecting to the database, so you can use any database supported by [sequelize] 
+
+You can pass the database configuration in `src/sql-config.json` file under the `production` key
+  
+ [sequelize]: <https://sequelize.org/master/manual/getting-started.html>
+ 
+# Api Key
+
+Set the google api key in `src/key.json` file under the `key` property.
+
+# Installation
+
+This script requires Node.js v10+ to run.
+Make sure `node` & `npm` are in the `path`
+
+Install the dependencies and devDependencies.
+```sh
+$ npm i
+```
+
+# Running the code
+
+### Location & Radius
+location & radius can be provided to the `main` function near the end of `src/app.ts` script.
+#### example:
+code can be run using the command:
+
+`main("-33.8670522,151.1957362", 1500)`
+
+# Run the code
+```sh
+npx ts-node src/app.ts 
+```
--- src/app.ts
+++ src/app.ts
@@ -5,24 +5,29 @@ import {
 
 import { Sequelize } from "sequelize-typescript";
 import { default as WebsiteModel } from "./models/website.model";
-
-// config
-const API_KEY = "AIzaSyCD_J38SJh8ho0mhSyA5GoQjZ1LVe3Ks5U";
-
-const sequelize = new Sequelize({
-  // config
-  models: [__dirname + "/models"],
-  // for mysql
-  //// put mysql config here!
-
-  // for sqlite
-  database: "some_db",
-  dialect: "sqlite",
-  storage: "db.sqlite"
-});
+var fs = require("fs");
 
 // globals
 const client = new Client({});
+
+// load API key
+const API_KEY = JSON.parse(fs.readFileSync(`${__dirname}/key.json`, "utf8"))
+  .key;
+
+// load sql config
+const sql_credentials = JSON.parse(
+  fs.readFileSync(`${__dirname}/sql-config.json`, "utf8")
+).development;
+
+// connect to db
+const sequelize = new Sequelize({
+  models: [__dirname + "/models"],
+  ...sql_credentials
+  // // for sqlite
+  // database: "some_db",
+  // dialect: "sqlite",
+  // storage: "db.sqlite"
+});
 
 /**
  * Get a website by place_id
@@ -51,36 +56,6 @@ const placeDetails = async (place_id: string) => {
  * @param radius
  */
 const getPlacesByLocation = async (location, radius: number) => {
-  // for debugging
-  // return [
-  //   // {
-  //   //   place_id: "ChIJO30UDHKV3zgR6S7lsn61fK0",
-  //   //   website: "https://www.shifa.com.pk/"
-  //   // },
-  //   {
-  //     place_id: "ChIJu74YtEOuEmsRZVq9lNEs8vc",
-  //     website: undefined
-  //   },
-  //   {
-  //     place_id: "ChIJ0zxfbzyuEmsRYYkBoKdBTP4",
-  //     website: "https:/www/sydney.frasershospitality.com/"
-  //   }
-  //   // {
-  //   //   place_id: "ChIJZ934S0KuEmsR_0lxV3PTR4M",
-  //   //   website: "https://www.fourseasons.com/sydney/?seo=google_local_syd1_apac"
-  //   // },
-  //   // {
-  //   //   place_id: "ChIJKR-RkzuuEmsR0IrnPnwUQRk",
-  //   //   website:
-  //   //     "https://www.panpacific.com/en/hotels-and-resorts/pr-darling-harbour-sydney/offers.html?utm_source=google&utm_medium=business_listing&utm_campaign=googlemybusiness"
-  //   // },
-  //   // {
-  //   //   place_id: "ChIJ4fN7mzyuEmsRKZ3ylJxaUHg",
-  //   //   website:
-  //   //     "https://www.radissonhotels.com/en-us/hotels/radisson-sydney?cid=a:se+b:gmb+c:apac+i:local+e:rad+d:row+h:AUSYDNEY"
-  //   // }
-  // ];
-
   // next page token
   let pagetoken: undefined | string = undefined;
 
@@ -117,26 +92,18 @@ const getPlacesByLocation = async (location, radius: number) => {
 
     place_website_list.push(...(await Promise.all(promises)));
 
-    // promises.push(placeDetails("ChIJO30UDHKV3zgR6S7lsn61fK0"));
-    // await new Promise(r => setTimeout(r, 2000));
-
     pagetoken = response.data.next_page_token;
   } while (pagetoken);
 
   return place_website_list;
 };
 
-const main = async () => {
+const main = async (location, radius: number) => {
   // sync and init the db
-  await sequelize.sync({
-    // force: true
-  });
+  await sequelize.sync({});
 
   // get the websites list
-  const place_website_list = await getPlacesByLocation(
-    "-33.8670522,151.1957362",
-    1500
-  );
+  const place_website_list = await getPlacesByLocation(location, radius);
 
   // filter places which have a website
   const filtered_data = place_website_list.filter(place_website => {
@@ -152,12 +119,13 @@ const main = async () => {
   WebsiteModel.bulkCreate(filtered_data, {
     fields: ["place_id", "website"],
     ignoreDuplicates: true
-    // updateOnDuplicate: ["place_id"]
   });
+
+  console.log(`execution completed`);
 };
 
 // run main
-main();
+main("-33.8670522,151.1957362", 1500);
 
-// //keep script alive
-// setInterval(() => {}, 1000);
+// keep the script alive for debugging
+setInterval(() => {}, 1000);
